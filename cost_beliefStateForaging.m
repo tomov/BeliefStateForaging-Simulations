5cacbf3db66351d8c778b3b98d7651d2ec964e51
Momchil Tomov <tomov90@gmail.com>
2019-06-05 22:24:19 -0400

    properly truncated gaussian for track 1 and track 2 (with min and max)

--- cost_beliefStateForaging.m
+++ cost_beliefStateForaging.m
@@ -3,5 +3,9 @@ function cost = cost_beliefStateForaging(x)
     %[simResults, i] = simulation_beliefStateForaging(x);
     [simResults, i] = formula_beliefStateForaging(x, false);
 
-    cost = simResults(i,2) / simResults(1,2);% + abs(0.9 - simResults(i,3));
+    cost = simResults(1,2) / simResults(i,2) + abs(0.9 - simResults(i,3)); % peaks and 90%
+    %cost = simResults(1,2) / simResults(i,2); % peaks only
+    %cost = abs(0.9 - simResults(i,3)); % 90% only
+
+
     cost = cost * 1000000;
--- formula_beliefStateForaging.m
+++ formula_beliefStateForaging.m
@@ -19,6 +19,9 @@ meanITI = x(3);
 mu = x(1); % mean of rew dist
 sigma = x(2); % std of rew dist
 
+min_dist = max(1, mu - 5 * sigma);
+max_dist = min(1000, mu + 5 * sigma);
+
 speed = 5; % AU per second
 
 track2maxRun = [10:10:300]; % distances to try for how far mouse is willing to run on track 2 before quiting
@@ -30,16 +33,23 @@ for iSim = 1:length(track2maxRun)
 
     frac_rew_npr = normcdf(stop_dist, mu, sigma); % fraction of rewarded track 2 non-probe trials (i.e. that have reward before stop_dist)
 
-    % mean of truncated gaussian -- see Moments: one-sided truncation (upper tail) https://en.wikipedia.org/wiki/Truncated_normal_distribution 
-    beta = (stop_dist - mu) / sigma;
-    mu_stop_dist = mu - sigma * normpdf(beta) / normcdf(beta);
+    % mean of truncated gaussian -- see Moments in https://en.wikipedia.org/wiki/Truncated_normal_distribution 
+    % track 1
+    alpha = (min_dist - mu) / sigma;
+    beta = (max_dist - mu) / sigma;
+    mu_tr1 = mu + sigma * (normpdf(alpha) - normpdf(beta)) / (normcdf(beta) - normcdf(alpha));
+    % track 2
+    alpha = (min_dist - mu) / sigma;
+    beta = (min(max_dist, stop_dist) - mu) / sigma; % stop distance taken into account
+    mu_tr2_npr_rew = mu + sigma * (normpdf(alpha) - normpdf(beta)) / (normcdf(beta) - normcdf(alpha));  % mean reward distance for track 2 rewarded trials
+
 
     % total reward = track 1 + track 2
     total_rew = 1 * n_tr1 + 1 * frac_rew_npr * n_tr2_npr;
 
     % total time = ITIs + track 1 times + ...
-    total_time = n * meanITI + (mu / speed) * n_tr1;
-    total_time = total_time + (mu_stop_dist/ speed) * frac_rew_npr * n_tr2_npr; %  ... +  track 2 rewarded times 
+    total_time = n * meanITI + (mu_tr1 / speed) * n_tr1;
+    total_time = total_time + (mu_tr2_npr_rew / speed) * frac_rew_npr * n_tr2_npr; %  ... +  track 2 rewarded times 
     total_time = total_time + stop_dist / speed * ((1 - frac_rew_npr) * n_tr2_npr + n_tr2_pr); % ... + track 2 non-rewarded times
 
     simResults(iSim,1) = iSim;
