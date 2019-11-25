c5632a9b3f1b06841546829888bbcbc220449175
Momchil Tomov <tomov90@gmail.com>
2019-11-25 13:44:05 -0500

    nice

--- demo.m
+++ demo.m
@@ -54,13 +54,16 @@ distr_params(9).mus = [50 350];
 distr_params(9).sigmas = [50 50];
 distr_params(9).w = [1 1];
 
-distr{10} = 'mixnorm';
+distr{10} = 'norm';
 x{10} = [80 30 8 0.7 0.2 10 150]; % mu, sigma, ITI, frac tr2, frac probe, min, max
 distr_params(10).mus = [20 40 60 80 100 120 140];
 distr_params(10).sigmas = 0.1 * ones(size(distr_params(10).mus));
 distr_params(10).w = normpdf(distr_params(10).mus, x{10}(1), x{10}(2));
+distr_params(10).mus = [];
+distr_params(10).sigmas = [];
+distr_params(10).w = [];
 
-gamma = 0.80;
+gamma = 0.60;
 d_dist = 0.1;
 frac_pr_tr1 = 0.01;
 speed = 10;
@@ -68,9 +71,9 @@ speed = 10;
 do_plot = true;
 
 for i = 10:10 % 1:length(distr)
-    analytical_optimal(x{i}, do_plot, distr{i}, distr_params(i), d_dist, frac_pr_tr1, gamma, speed);
+    %analytical_optimal(x{i}, do_plot, distr{i}, distr_params(i), d_dist, frac_pr_tr1, gamma, speed);
 
     %analytical_TD(x{i}, do_plot, distr{i}, distr_params(i), d_dist, frac_pr_tr1, gamma, speed);
     %analytical_hazard(x{i}, do_plot, distr{i}, distr_params(i), d_dist, frac_pr_tr1, gamma, speed);
-    %analytical_beliefTD(x{i}, do_plot, distr{i}, distr_params(i), d_dist, frac_pr_tr1, gamma, speed);
+    analytical_beliefTD(x{i}, do_plot, distr{i}, distr_params(i), d_dist, frac_pr_tr1, gamma, speed);
 end
